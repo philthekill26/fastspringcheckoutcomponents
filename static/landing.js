4274c6c5e986479dff92be5236a949e163f04ac3
philthekill26 <84179350+philthekill26@users.noreply.github.com>
2026-09-08 11:56:29 +0100

    Delete static/landing.js

--- static/landing.js
+++
@@ -1,7 +0,0 @@
-const planButtons = document.querySelectorAll(".select-plan-button");
-
-planButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    window.location.href = "/checkout";
-  });
-});
