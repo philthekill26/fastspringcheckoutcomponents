19b6780c9e42033869629c327362f7a47e8c9ade
philthekill26 <84179350+philthekill26@users.noreply.github.com>
2026-09-08 10:06:27 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -46,6 +46,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def home() -> FileResponse:
+    return FileResponse("static/landing.html")
+
+
+@app.get("/checkout")
+def checkout_page() -> FileResponse:
     return FileResponse("static/components.html")
 
 
